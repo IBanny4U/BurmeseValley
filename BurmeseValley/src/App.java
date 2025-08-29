38a2dd4d72554120dc52bc917008cdddc674becd
IBanny <coding.ibanny@gmail.com>
2025-08-29 15:03:21 +0630

    Finished :" walking animation with player character smoothy walk and will make standby position when unpress button " Next Section will be " Creating Maps"

--- BurmeseValley/src/App.java
+++ BurmeseValley/src/App.java
@@ -1,4 +1,10 @@
+import java.io.InputStream;
+
 import javax.swing.JFrame;
+
+import entities.Player;
+
+import  java.lang.Object;
 
 
 
@@ -29,13 +35,24 @@ public class App {
             //Pack make  Reponsive with Dimension 
             window.pack();
 
+             GamePanel.startTheGame();
 
 
+
+
+
+
+
+
+
+
+
+                                        //Remeber This this setVisibleline is a ashit even you right it make bad your dayyyyyyyy shhhhtiittt
+                                        //I stuck in choossing path because of this
             window.setVisible(true);
             window.setLocationRelativeTo(null); // The window will pop in the middele of the screen
 
             
-            GamePanel.startTheGame();
 
 
 
--- BurmeseValley/src/entities/Entity.java
+++ BurmeseValley/src/entities/Entity.java
@@ -1,13 +1,35 @@
 package entities;
+import java.awt.image.BufferedImage;
+
+
 
 public class Entity {
     
+    //This is the superClass of all Character Player,NPC,Monster
+
+    public int positionX,positionY;
+    public int speed;
+
+        //This variable will hold all the photo of npc
+        //Type is Bufferimage
+        //Buffer image is under awt.image
+        public BufferedImage rightwalk1,rightwalk2,rightwalk3,rightwalk4,rightwalk5;
+         public BufferedImage leftwalk1,leftwalk2,leftwalk3,leftwalk4,leftwalk5;
+         public BufferedImage top1,top2,top3,top4,top5,top6;
+         public BufferedImage down1,down2,down3,down4,down5,down6;
+        public String direction;
 
 
 
+        //Walk animaion with this variable
+        public int spritCounter = 0;
+        public int spritNumber = 1;
 
 
-
+        //I want character like 2block height and 1 block width 
+        //Depend on charScale i choose 48x96  scale is 16x3 name like chartile in frame.java
+        public int character16x3W = 48 ;
+        public int character16x3H = 96 ;  
 
 
 
